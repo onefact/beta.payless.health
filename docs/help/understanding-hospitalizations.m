192bccabbe5c5b30cddd73e57727dc86ac4f51a1
kimweiss11 <131694597+kimweiss11@users.noreply.github.com>
2023-08-28 16:05:49 -0500

    Create understanding-hospitalizations.m
    created file and added text
    
    Signed-off-by: kimweiss11 <131694597+kimweiss11@users.noreply.github.com>

---
+++ docs/help/understanding-hospitalizations.m
@@ -0,0 +1,63 @@
+**Understanding Hospitalization: Admission, Patient Rights, Discharge and Follow-up Care**
+
+**Author: Lauren Wheeler, MD, BCPA**
+
+**Purpose of this guidance:** Hospitalization can be overwhelming for both patients and family members, but knowledge of the process can alleviate some of that stress. This chapter walks you through the admission and discharge processes and also tells you what your rights are as a patient in the hospital. 
+
+Millions of people are admitted to the hospital every year, meaning they stay in a hospital bed and are cared for by hospital staff. [Choosing a hospital](https://medlineplus.gov/healthfacilities.html) may be a matter of preference, distance, quality, or cost.
+
+Although people come to the hospital for many reasons and all hospitals are a little different, some things are mostly the same. Being in the hospital can feel scary, but knowing what to expect can help. 
+
+**What Is the Hospital Admission Process?**
+
+Being [admitted to the hospital](https://healthadministrationdegree.usc.edu/blog/what-to-expect-when-admitted-to-the-hospital/) can happen in a couple of ways. If there is an emergency, the patient will often be admitted through the emergency room. Sometimes though, a doctor will directly admit a patient to the hospital from their clinic or as part of a care plan. This can happen with certain tests or treatments that need to be done in-patient (in the hospital), like chemotherapy. 
+
+At some point during the process, the hospital will “register” the patient, meaning they collect information like their name, birthday, insurance, and address, and they usually ask the patient to sign a form to give the hospital permission to treat the patient. 
+
+The next step is usually a bed assignment—someone has to make sure there is an empty bed in the building for the patient with enough nurses to take care of them.
+
+At some point, a doctor will come see the patient to get to know their medical history and find out about what brought them to the hospital. This is called an **admission history and physical** (H&P). It usually includes a plan of what will happen during the hospital stay.
+
+**What Are the Rights of a Hospitalized Patient?** 
+
+While a person is in the hospital, many things will feel and work differently. Often the person isn’t in their own clothes, they don’t have their familiar surroundings, and they aren’t feeling well. In addition, hospital staff will frequently come into the room, including technicians, nurses, doctors, students, dieticians, and more.
+
+It can be easy for a hospitalized person to feel helpless or as if they don’t have any say in what happens to them. However, hospitalized patients definitely have rights, including the right to:
+
+- Respect: Patients have the right to be treated [respectfully](https://code-medical-ethics.ama-assn.org/ethics-opinions/patient-rights) and with dignity.
+- Information: Patients have a right to know what their doctors think is going on with their conditions and what they believe the best treatment may be. 
+- Make decisions: Patients have the right to make decisions about what kinds of tests and treatments they want or don’t want. Patients can refuse any and all treatment.
+- Leave: Patients are allowed to leave the hospital when they want, with a few legal exceptions. 
+- Privacy: Patients have the right to control who reads their medical [records](https://www.hhs.gov/hipaa/for-individuals/guidance-materials-for-consumers/index.html) and which friends and family are allowed into their hospital room. They have the right to a copy of their records and to request changes of inaccurate information in their records.
+- Safety: Patients should be safe from medical errors in the hospital. They should receive the [right medications](https://medlineplus.gov/ency/patientinstructions/000501.htm) and have the [right surgeries](https://medlineplus.gov/ency/patientinstructions/000618.htm) on the correct part of the body.  
+
+Most hospitals also have a [patient bill of rights](https://www.americanpatient.org/what-are-your-rights-as-a-patient/) which should be given to the patient at admission. If the patient has any trouble, there is usually someone who works at the hospital who can help—this person might be called an ombudsman, a patient relations expert, or a patient advocate. Safety concerns can also be reported to [The Joint Commission](https://www.jointcommission.org/resources/patient-safety-topics/report-a-patient-safety-concern-or-complaint/), which is the agency in charge of all hospitals in the US.
+
+**What Is a Safe Hospital Discharge? What Are Hospital Discharge Papers?**
+
+Hospital [discharge planning](https://eldercare.acl.gov/Public/Resources/Factsheets/Hospital_Discharge.aspx), or planning for the patient to leave the hospital, starts as soon as the patient is admitted. Often a social worker and/or nurse case manager will help coordinate things like making follow-up appointments, getting new equipment (like a wheelchair or oxygen tank), or finding a place to stay after the hospital, like an in-patient rehabilitation facility.  
+
+A safe hospital discharge means that when you leave the hospital, you know [where you will go](https://medlineplus.gov/ency/patientinstructions/000867.htm) and what you need to do to be healthy. It means the patient goes to a safe place where all of their needs can be met—whether that is home, a relative’s home, or another facility like a nursing home. Around [12% of people](https://psnet.ahrq.gov/perspective/patient-safety-during-hospital-discharge) have some negative health event within 3 weeks after leaving the hospital that could have been prevented by better communication.
+
+To improve communication between the hospital team and the outpatient team (the doctors, caregivers, and others who help the patient), hospitals try to make sure people leave with discharge papers. Discharge papers are [written instructions](https://www.ahrq.gov/questions/resources/going-home/index.html) for the patient and their family members to make sure they know everything they need about what happens next. They should include information about:
+
+- Diet: What foods should I eat? Do I have swallowing restrictions?
+- Activity: Am I allowed to exercise? Is it safe to lift heavy objects?  
+- Symptoms: When should I return to the hospital? What symptoms should I report and to who?
+- Medications: Do I have new medications? Where will I pick them up? What will they cost?
+- Follow-up appointments: Which doctors should I see after I leave the hospital?  
+
+If there’s anything that is unclear or that would be difficult for you to do, speak up. Often the [people who work in the hospital](https://www.ncbi.nlm.nih.gov/books/NBK259995/) don’t know what the home situation will be like for the patient—their friends and family do.
+
+**What Do I Need to Know About Hospital Follow-up Appointments?** 
+
+Because being in the hospital shows that you experienced a health crisis, you likely need a little extra support to stay healthy once you leave the hospital. The hospital follow-up appointment is a chance to meet with your regular doctor (your primary care provider) and talk about what happened, both in the hospital and since you’ve left. If you don’t have a regular doctor, now’s a great time to find one. It’s a great opportunity to:
+
+- Discuss how your health has changed and watch for any concerning symptoms
+- Go over medications
+- Review results that weren’t available when you left the hospital
+- Talk about how to prevent going back to the hospital
+- Ask about more resources to help you stay healthy or do the things that the hospital doctor told you to do
+- Discuss your wishes for end-of-life care
+
+Research has shown that hospital follow-up appointments within [30 days](https://bmchealthservres.biomedcentral.com/articles/10.1186/s12913-021-06848-9) of leaving the hospital can help prevent negative health events, like going back to the hospital (readmission) or [even death](https://doi.org/10.1001/jamanetworkopen.2020.13100). This is [especially true](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4369604/) for higher-risk patients, who tend to be older and have more chronic health conditions. These higher-risk patients should ideally have a follow-up appointment within 7 days of leaving the hospital. If your hospital team didn’t schedule you a follow-up appointment, take the initiative to schedule one yourself.
